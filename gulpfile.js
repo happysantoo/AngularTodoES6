320f4f9067e6d334a83bd7e82c78469ca73a68fb
Santhosh Kuppusamy <santoo.k@gmail.com>
2014-05-07 10:43:07 -0500

    make it run

--- gulpfile.js
+++ gulpfile.js
@@ -11,16 +11,16 @@ gulp.task('default', function() {
     watch({glob: 'app/**/*.js',emitOnGlob:false, verbose:true})
             .pipe(plumber())
             .pipe(traceur({sourceMap: true,experimental: true}))
-            .pipe(gulp.dest('out'));
+            .pipe(gulp.dest('out/app'));
 });
 
 gulp.task('clean', function () {
-    return gulp.src('out', {read: false})
+    return gulp.src('out/app', {read: false})
         .pipe(clean());
 });
 
 gulp.task('transpile',function(){
     return gulp.src('app/**/*.js')
         .pipe(traceur({sourceMap: true,experimental: true}))
-        .pipe(gulp.dest('out'));
+        .pipe(gulp.dest('out/app'));
 });
